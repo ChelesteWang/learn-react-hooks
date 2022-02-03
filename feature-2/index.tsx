b83e886b706f610f0413e1930748d3b4086cfd2c
ChelesteWang <1581446178@qq.com>
2022-02-03 16:01:03 +0800

    1 file modified

--- feature-2/index.tsx
+++ feature-2/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Counter from './count';
+import User from './user';
 
 export default function () {
   return (
     <div>
       <h2>feature-2</h2>
       <Counter />
+      <User />
     </div>
   );
 }

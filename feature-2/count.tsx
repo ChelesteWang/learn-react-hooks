6806ad3332763e5460ee7efa3b91a20a97899bc2
ChelesteWang <1581446178@qq.com>
2022-02-03 19:18:23 +0800

    1 file modified

--- feature-2/count.tsx
+++ feature-2/count.tsx
@@ -4,9 +4,10 @@ export default function () {
   const [count, setCount] = useState(0);
 
   useEffect(() => {
-    setTimeout(() => {
+    const timmer = setTimeout(() => {
       setCount(() => count + 1);
     }, 3000);
+    return clearTimeout(timmer);
   });
 
   return <div>三秒后+1：{count}</div>;

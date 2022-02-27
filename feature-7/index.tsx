8b8c73f9f01b11db34e6bf99c98e8202ef3d4a06
ChelesteWang <1581446178@qq.com>
2022-02-27 16:00:51 +0800

    1 file modified

--- feature-7/index.tsx
+++ feature-7/index.tsx
@@ -1,8 +1,13 @@
 import Timer from './timer';
+import Store from './useCount';
 import React from 'react';
 
 function App() {
-  return <Timer />;
+  return (
+    <Store.Provider>
+      <Timer />
+    </Store.Provider>
+  );
 }
 
 export default App;
